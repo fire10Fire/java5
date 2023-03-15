7a39e666d5a860fc2f5974a6cb5b7f1484b51b5f
Mucahit Akbas <akbasmucahit10@gmail.com>
2023-03-15 16:36:33 +0300

    first commit

---
+++ UceDordeBöl/Ortalamam.java
@@ -0,0 +1,25 @@
+package UceDordeBöl;
+import java.util.Scanner;
+public class Ortalamam {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("sayı giriniz : ");
+        int say = in.nextInt();
+        int i = 1 ;
+        int j = 0; 
+        int toplam = 0;
+        double ortalama = 0;
+        while(say>=i){
+            boolean art =(i%3==0)||(i%4==0);
+            if(art){
+                j++;
+                 toplam =i+toplam;
+                 System.out.print(i+", ");
+                }
+                i++;
+            }
+            ortalama = toplam/j;
+        System.out.println(" Ortalama : "+ ortalama);
+
+    }
+}
