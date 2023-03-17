1d2a69b08b45fa0ef0631e55862243159e6468af
Mucahit Akbas <akbasmucahit10@gmail.com>
2023-03-17 15:03:22 +0300

    ufak eklenti

--- DörtVeBesinKatı/DortVeBes.java
+++ DörtVeBesinKatı/DortVeBes.java
@@ -8,13 +8,13 @@ public class DortVeBes {
     System.out.print("Sınır değeri yazınız : ");
     int ila = 0; 
     ila = in.nextInt();
-    
+
     System.out.print("dördün katları : ");
-    for(int a =1 ; ila>a*4; a++ ){
+    for(int a =1 ; ila>=a*4; a++ ){
         System.out.print(a*4 + ", ");
     }
     System.out.print("\nBeşin katları : ");
-    for(int a =1 ; ila>a*5; a++ ){
+    for(int a =1 ; ila>=a*5; a++ ){
         System.out.print(a*5 + ", ");
     }
     }
