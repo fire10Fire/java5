b3f2e680a1ce18700e8b83272f866ef932dca912
Mucahit Akbas <akbasmucahit10@gmail.com>
2023-03-19 16:39:22 +0300

    şimdi daha güzel ayrıca üstoplamdakileri toplama boşaltma mekanizmasını sevdim.

--- ArmstrongSayıları/Armstrong.java
+++ ArmstrongSayıları/Armstrong.java
@@ -9,31 +9,33 @@ public class Armstrong {
             System.out.println("Sayı giriniz : ");
             int sayı = in.nextInt();
             int gecSayı = sayı;
-            int gec2Sayı=sayı;
+            int üsToplam=1;
             int kalan = 0;
             int basSayı=0;
             int toplam=0;
 
             int sayıcı=0;
             // basamak sayısını hesaplar
-            for(int i=1;gecSayı>0;i++){
-                gecSayı /=10;  
+            while(gecSayı!=0){
+                gecSayı/=10;
                 sayıcı++;
-            } 
+            }
+            gecSayı=sayı;
+            
             System.out.println("basamak sayısı : "+ sayıcı);
             basSayı=sayıcı;
 
             for(int i =1;basSayı>=i;i++){
-                kalan = gec2Sayı%10;
-                System.out.println("basamağın değer :"+ kalan);
-                int üsToplam=1;
-                gec2Sayı=gec2Sayı/10;
-                    for(int j=1;basSayı>=j;j++){
-                        üsToplam *= kalan;
-                        System.out.println("üsToplam"+üsToplam);
-                    }
-
-                    toplam+=üsToplam;
+                kalan = gecSayı%10;
+                
+                üsToplam=1;
+                for(int j=1;basSayı>=j;j++){
+                    üsToplam *= kalan;
+                    System.out.println("üsToplam"+üsToplam);
+                }
+                
+                gecSayı=gecSayı/10;
+                toplam+=üsToplam;
                 }
                 System.out.println("toplam : " + toplam);
                 if(toplam==sayı){
